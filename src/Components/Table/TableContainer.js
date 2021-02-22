d038f442830c3ab79bd2fa15c6ff351b6f4e3bc9
Eugenia Kurmeneva <e.kurmeneva@gmail.com>
2021-02-22 21:25:51 +0300

    Add the table's structure and markup

--- src/Components/Table/TableContainer.js
+++ src/Components/Table/TableContainer.js
@@ -1,9 +1,53 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useMemo} from "react";
 
 import getData from "../../lib/getData";
+import Table from "./Table";
 
 function TableContainer() {
   const [data, setData] = useState([]);
+  const columns = useMemo(
+    () => [
+      {
+        Header: "General",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "name"
+          },
+          {
+            Header: "Birth year",
+            accessor: "birth_year"
+          },
+          {
+            Header: "Gender",
+            accessor: "gender"
+          }
+        ]
+      },
+      {
+        Header: "Physical appearance",
+        columns: [
+          {
+            Header: "Height",
+            accessor: "height"
+          },
+          {
+            Header: "Mass",
+            accessor: "mass"
+          },
+          {
+            Header: "Hair color",
+            accessor: "hair_color"
+          },
+          {
+            Header: "Skin color",
+            accessor: "skin_color"
+          }
+        ]
+      }
+    ],
+    []
+  );
 
   useEffect(() => {
     getData().then((res) => {
@@ -14,9 +58,7 @@ function TableContainer() {
   console.log(data);
 
   return (
-    <div>
-      the table container
-    </div>
+    <Table columns={columns} data={data} />
   );
 }
 
